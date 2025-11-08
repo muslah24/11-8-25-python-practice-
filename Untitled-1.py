9b59514e5ea3ed1d19310e4859269f9fda034ba3
Musleh uddin <156533883+muslah24@users.noreply.github.com>
2025-11-08 21:39:36 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+name = "Musleh"
+age = 20
+hight = 5.9
+Is_student = True
+
+#print(type(name))
+print(type(age))
---
+++ hello.py
@@ -0,0 +1,18 @@
+# define variables of different data types
+integer_var = 42
+float_var = 3.14
+string_var = "hello word"
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+dict_var = {"name": "Alice" , "role": "Engineer" }
+bolean_var = True
+
+# print and manipulate the variables
+print("Integer:", integer_var)
+print("Float:", float_var)
+print("String:", string_var+ " bootcamp")
+list_var.append(4)
+print("List:", list_var)
+print("Tuple:", tuple_var + (7,8))
+print("Dictionary:", dict_var)
+print("Boolean:", bolean_var)
