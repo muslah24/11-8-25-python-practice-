f96b35050563a9cd4ec2fdb1734920368f14b382
Musleh uddin <156533883+muslah24@users.noreply.github.com>
2025-11-08 23:45:45 +0600

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,13 @@
+#for i in range(5):
+#    print(i)
+#count = 5
+#count = 0
+#count = 0
+#while count < 5:
+#   print(count) 
+#  count += 1
+
+def greet(name):
+    print("Hello " + name + ", Good Morning!")
+
+greet("Musleh")
